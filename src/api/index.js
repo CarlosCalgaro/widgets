02600b89b34e8e1e3b15e3af4af837dbdd3c5e06
Carlos Calgaro <carloscalgarof@gmail.com>
2020-03-29 22:32:32 +0100

    Added Widgets to the api object

--- src/api/index.js
+++ src/api/index.js
@@ -1,6 +1,7 @@
 import AuthenticationApi from 'src/api/Authentication'
-
+import WidgetsApi from 'src/api/Widgets'
 
 export default {
     Authentication: new AuthenticationApi(),
+    Widgets: new WidgetsApi()
 }
