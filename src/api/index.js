e0d98c3bb8bba5856ecf0552cde58c39c2e8a2e8
Carlos Calgaro <carloscalgarof@gmail.com>
2020-03-31 03:44:47 +0100

    Creation of User Api

--- src/api/index.js
+++ src/api/index.js
@@ -1,7 +1,9 @@
 import AuthenticationApi from 'src/api/Authentication'
 import WidgetsApi from 'src/api/Widgets'
+import UsersApi from 'src/api/Users'
 
 export default {
     Authentication: new AuthenticationApi(),
-    Widgets: new WidgetsApi()
+    Widgets: new WidgetsApi(),
+    Users: new UsersApi()
 }
