0293d572c0aad1cbc7c6c3841c60cc3ba97597b6
Carlos Calgaro <carloscalgarof@gmail.com>
2020-03-31 03:38:24 +0100

    Refctoring modalform to better adapt to redux

--- src/components/ModalForm/index.js
+++ src/components/ModalForm/index.js
@@ -2,46 +2,37 @@ import React from 'react';
 import {
     Modal
   } from "reactstrap";
-import classnames from "classnames";
+import {connect} from "react-redux";
+import * as actions from 'src/store/actions/modals';
   
 
 class ModalForm extends  React.Component {
 
     constructor(props){
-        super(props);
-        this.state = {
-            formModal: false
-        }
+      super(props);
+      this.toggleModal = this.toggleModal.bind(this);
     }
-
-    componentDidUpdate(prevProps) {
-        if (this.props.open !== prevProps.open) {
-            this.toggleModal("formModal")
-        }
-    }
-
-    toggleModal = modalState => {
-        this.setState({
-          [modalState]: !this.state[modalState]
-        });
+    
+    toggleModal(){
+      this.props.actions.toggle(this.props.name)
     };
 
     render(){
       return(
           <Modal
             modalClassName="modal-black"
-            isOpen={this.state.formModal}
-            toggle={() => this.toggleModal("formModal")}
+            isOpen={this.props.modals[this.props.name]}
+            toggle={this.toggleModal}
           >
             <div className="modal-header justify-content-center">
               <button
                 className="close"
-                onClick={() => this.toggleModal("formModal")}
+                onClick={this.toggleModal}
               >
                 <i className="tim-icons icon-simple-remove text-white" />
               </button>
               <div className="text-muted text-center ml-auto mr-auto">
-                <h3 className="mb-0">Sign in with</h3>
+                <h3 className="mb-0">{this.props.description}</h3>
               </div>
             </div>
             <div className="modal-body">
@@ -52,4 +43,23 @@ class ModalForm extends  React.Component {
     }
 }
 
-export default ModalForm;
+const mapStateToProps = (state) => {
+  return {
+    modals: state.modals
+  };
+}
+
+const mapDispatchToProps = (dispatch) => {
+  return {
+      actions: {
+          toggle: (modal) => {
+              return dispatch({
+                  type: actions.TOGGLE,
+                  payload: modal
+              })
+          }
+      }
+  }
+}
+
+export default connect(mapStateToProps, mapDispatchToProps)(ModalForm);
