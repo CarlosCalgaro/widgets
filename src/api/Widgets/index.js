cf71a278439c13b17c4f25096c43cbdc77e4822f
Carlos Calgaro <carloscalgarof@gmail.com>
2020-03-31 03:47:25 +0100

    Added some endpoints to the widget API

--- src/api/Widgets/index.js
+++ src/api/Widgets/index.js
@@ -1,10 +1,9 @@
 import ApiBase from "src/api/ApiBase";
-import {VISIBLE_WIDGETS_URL} from 'src/api/ApiConstants'
+import {VISIBLE_WIDGETS_URL, WIDGETS_URL} from 'src/api/ApiConstants'
 
 class WidgetsApi extends ApiBase {
 
     async visible(term=""){
-        console.log("Executed with term:" + term)
         let request = {
                 method: 'get',
                 url: this.api_namespaced_url + VISIBLE_WIDGETS_URL,
@@ -15,6 +14,36 @@ class WidgetsApi extends ApiBase {
             request.params = { term: term }
         }   
         return await this.sendRequest(request)
+    }
+
+    async index(){
+        let request = {
+            method: 'get',
+            url: this.api_namespaced_url + WIDGETS_URL,
+            headers: this.headers()
+        }
+        return await this.sendRequest(request);
+    }
+
+    async destroy(id){
+        let request = {
+            method: 'delete',
+            headers: this.headers(),
+            url: this.api_namespaced_url + WIDGETS_URL + "/" + id
+        }
+        return await this.sendRequest(request);
+    }
+
+    async create(data){
+        let request = {
+            method: 'post',
+            url: this.api_namespaced_url + WIDGETS_URL,
+            headers: this.headers(),
+            data: {
+                widget: data
+            }
+        }
+        return await this.sendRequest(request);
     }
 }
 
